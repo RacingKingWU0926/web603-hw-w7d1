2bd4d68034a60fc07f280ac1ba4a40c522524189
JasonLei <jlei@activision.com>
2024-02-24 13:55:09 -0800

    complete providing a theme

--- theme-app/src/App.js
+++ theme-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
 
-function App() {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+`;
+
+const SwitchTheme = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 75px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <ThemeProvider theme={{ theme: "light" }}>
+        <SwitchTheme>
+          <h1>Theme App</h1>
+          <p>
+            <input type="checkbox" />Use Dark Theme
+          </p>
+        </SwitchTheme>
+        <Container>
+          <h2>Welcome!</h2>
+          <h3>Full Stack Web Development</h3>
+        </Container>
+      </ThemeProvider>
+    </React.Fragment>
+  )
 }
 
 export default App;
